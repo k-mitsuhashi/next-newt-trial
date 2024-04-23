569db355ec3395d18078a74153edc9afee0dc389
k-mitsuhashi <sapphire_blue_1982@yahoo.co.jp>
2024-04-23 22:21:09 +0900

    Add sample page

---
+++ src/app/helloworld/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Hellow world",
+  description: "Hellow world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="jp">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/helloworld/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <div>はろーわーるど</div>
+    </main>
+  );
+}
